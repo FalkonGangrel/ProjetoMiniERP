7d75079c4cca4577639adef0b17480085577947b
FalkonGangrel <falkon.gangrel@gmail.com>
2025-05-28 20:57:12 -0300

    Update ProdutoController.php

--- app/Controllers/ProdutoController.php
+++ app/Controllers/ProdutoController.php
@@ -8,8 +8,8 @@ class ProdutoController
     {
         // Simulação de dados
         $produtos = [
-            ['id' => 1, 'nome' => 'Produto A', 'preco' => 10.99],
-            ['id' => 2, 'nome' => 'Produto B', 'preco' => 20.49],
+            ['id' => 1, 'nome' => 'Produto A', 'preco' => 10.99, 'estoque' => 20],
+            ['id' => 2, 'nome' => 'Produto B', 'preco' => 20.49, 'estoque' => 10],
         ];
 
         view('produtos/lista', [
