6e5335fa867b27978a7fbcd7fd17a4a3a19d7f20
FalkonGangrel <falkon.gangrel@gmail.com>
2025-06-10 23:57:23 -0300

    Atualizar rotas

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,29 @@
 return [
     '/' => 'HomeController@index',
 
+    // Produtos
     '/produtos' => 'ProdutoController@listar',
     '/produtos/novo' => 'ProdutoController@cadastro',
     '/produtos/salvar' => 'ProdutoController@salvar',
-    '/produtos/editar' => 'ProdutoController@editar',
-'/produtos/atualizar' => 'ProdutoController@atualizar',
+    '/produtos/editar/{id}' => 'ProdutoController@editar',
+    '/produtos/atualizar' => 'ProdutoController@atualizar',
 
+    // Estoques
     '/estoques' => 'EstoqueController@listar',
     '/estoques/novo' => 'EstoqueController@cadastro',
     '/estoques/salvar' => 'EstoqueController@salvar',
 
+    // Pedidos
     '/pedidos' => 'PedidoController@listar',
-    '/pedidos/criar' => 'PedidoController@carrinho',
+    '/pedidos/adicionar/{id}' => 'PedidoController@adicionarCarrinho',
+    '/pedidos/remover/{id}' => 'PedidoController@removerCarrinho',
+    '/pedidos/carrinho' => 'PedidoController@carrinho',
+    '/pedidos/finalizar' => 'PedidoController@finalizar',
+    '/pedidos/confirmar' => 'PedidoController@confirmar',
 
+    // Cupons (opcional)
+    '/cupons/validar/{codigo}' => 'CupomController@validar',
+
+    // Relatórios
     '/relatorios' => 'RelatorioController@exibir',
 ];
