a1581385698f2fb868d0753f5c4e063788ef0b99
FalkonGangrel <falkon.gangrel@gmail.com>
2025-08-22 16:36:13 -0300

    feat(index): adicionar tela inicial do sistema

---
+++ app/Views/home/index.php
@@ -0,0 +1,14 @@
+<h1 class="mb-4"><?=$title;?></h1>
+<p class="lead">Escolha uma das opções abaixo para começar:</p>
+
+<div class="row mt-5">
+    <div class="col-md-4">
+        <a href="/clientes" class="btn btn-primary btn-lg w-100 mb-3">Gerenciar Clientes</a>
+    </div>
+    <div class="col-md-4">
+        <a href="/produtos" class="btn btn-success btn-lg w-100 mb-3">Gerenciar Produtos</a>
+    </div>
+    <div class="col-md-4">
+        <a href="/relatorios" class="btn btn-secondary btn-lg w-100 mb-3">Relatórios</a>
+    </div>
+</div>
